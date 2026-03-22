21e13d5bd6600afdbb47cd2473bb44012085afe3
MrurBo <mrurbo2011@gmail.com>
2026-03-22 23:18:41 +0000

    ihnpmv3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express 🚀");
+  res.send("Hello from Express");
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
 });
