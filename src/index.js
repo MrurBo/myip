d20664732c4e32ac5eb1e2370bda4f3577a1d151
MrurBo <mrurbo2011@gmail.com>
2026-03-23 18:54:05 +0000

    FIXED EVERYTHING, adding functionality

--- src/index.js
+++ src/index.js
@@ -3,9 +3,44 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express");
+  const ip = req.ip;
+
+  if (req.isApiClient) {
+    return res.send(ip);
+  }
+
+  return res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <style>
+          body {
+            margin: 0;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: sans-serif;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>${ip}</h1>
+      </body>
+    </html>
+  `);
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+app.use((req, res, next) => {
+  const accept = req.headers.accept || '';
+  const ua = req.headers['user-agent'] || '';
+
+  const wantsJSON = accept.includes('application/json');
+  const looksLikeBrowser = /Mozilla|Chrome|Safari|Firefox|Edge/.test(ua);
+
+  req.isApiClient = wantsJSON && !looksLikeBrowser;
+
+  next();
 });
+
+app.listen(3000, "0.0.0.0", () => {});
